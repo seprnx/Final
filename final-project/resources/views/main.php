43c13b529ae47599e9f4e4159729baa3ae5bf925
William Ma <mqcuong1603@gmail.com>
2024-04-27 10:40:00 +0700

    hh

---
+++ final-project/resources/views/main.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Divided Website</title>
+</head>
+<body>
+    <div style="display: flex;">
+        <div style="width: 50%;">
+            <h2>Employee Section</h2>
+            <p>Click <a href="employee-login.php">here</a> to login as an employee.</p>
+        </div>
+        <div style="width: 50%;">
+            <h2>Admin Section</h2>
+            <p>Click <a href="admin-login.php">here</a> to login as an admin.</p>
+        </div>
+    </div>
+</body>
+</html>
